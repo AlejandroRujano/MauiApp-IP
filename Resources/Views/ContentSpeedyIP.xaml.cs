b9edd57226dcb1849233e2df841be74750e02eb8
Lukar10Y <lukariXU01@gmail.com>
2023-06-20 16:57:49 -0400

    Actualizacion 20_06_2023

--- Resources/Views/ContentSpeedyIP.xaml.cs
+++ Resources/Views/ContentSpeedyIP.xaml.cs
@@ -9,9 +9,11 @@ public partial class ContentSpeedyIP : ContentPage
     public ContentSpeedyIP()
     {
         InitializeComponent();
+
+
     }
 
-    protected override void OnAppearing()
+    protected async override void OnAppearing()
     {
         base.OnAppearing();
 
@@ -23,11 +25,45 @@ public partial class ContentSpeedyIP : ContentPage
 
         GridSuperior.BindingContext = _adaptadorActivo;
 
+        GridDesplegable.BindingContext = _adaptadorActivo;
+
+        //ImgCheck.Opacity = 0;
+
+        await ImgCheck.ScaleTo(1.25, 2500, Easing.Linear);
+        //ImgCheck.FadeTo(1, 1000, Easing.Linear);
+        await ImgCheck.ScaleTo(1, 1200, Easing.Linear);
+        
+
         //BindableLayout.SetItemsSource(GridSuperior, _adaptadorActivo);
     }
-
     private void BtnRegresar_Clicked(object sender, EventArgs e)
     {
         Shell.Current.GoToAsync("..");
+    }
+
+    private async void BtnMostrasMas_Clicked(object sender, EventArgs e)
+    {
+        if (!GridDesplegable.IsVisible)
+        {
+            GridDesplegable.Opacity = 1;
+            VerticalDesplegable1.Opacity = 0;
+            VerticalDesplegable2.Opacity = 0;
+            VerticalDesplegable3.Opacity = 0;
+            VerticalDesplegable4.Opacity = 0;
+            VerticalDesplegable5.Opacity = 0;
+
+            GridDesplegable.IsVisible = true;
+
+            await VerticalDesplegable1.FadeTo(1, 200, Easing.Linear);
+            await VerticalDesplegable2.FadeTo(1, 200, Easing.Linear);
+            await VerticalDesplegable3.FadeTo(1, 200, Easing.Linear);
+            await VerticalDesplegable4.FadeTo(1, 200, Easing.Linear);
+            await VerticalDesplegable5.FadeTo(1, 200, Easing.Linear);
+        }
+        else
+        {
+            await GridDesplegable.FadeTo(0, 200, Easing.Linear);
+            GridDesplegable.IsVisible = false;
+        }
     }
 }
